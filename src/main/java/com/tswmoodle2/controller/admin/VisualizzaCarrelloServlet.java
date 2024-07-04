f72b6839853f1026b21e16204c0bc467a67b716b
your-id <y.dashchuk@studenti.unisa.it>
2024-07-05 00:50:18 +0200

    carrello

--- src/main/java/com/tswmoodle2/controller/admin/VisualizzaCarrelloServlet.java
+++ src/main/java/com/tswmoodle2/controller/admin/VisualizzaCarrelloServlet.java
@@ -6,14 +6,28 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.beans.Carrello;
+import model.dao.CartDaoImpl;
+
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "VisualizzaCarrelloServlet", value = "/VisualizzaCarrelloServlet")
 public class VisualizzaCarrelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String elemento=request.getParameter("id");
-        request.setAttribute("elemento", elemento);
-System.out.println("visual carrello servlet");
+        String userId=request.getParameter("id");
+        request.setAttribute("elemento", userId);
+        String courseId=request.getParameter("productId");
+        if(courseId!=null) {
+            CartDaoImpl c=new CartDaoImpl();
+            try {
+                c.deleteFromCarrello(c.getCartIDByUser(Integer.parseInt(userId)), Integer.parseInt(userId), Integer.parseInt(courseId));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/results/admin/modificaCarrello.jsp");
+            rd.forward(request, response);
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/results/admin/modificaCarrello.jsp");
         rd.forward(request, response);
     }
