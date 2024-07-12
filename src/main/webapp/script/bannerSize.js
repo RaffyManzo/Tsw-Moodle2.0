35c0641a4502514c2099206a56205b8e9952c49a
your-id <y.dashchuk@studenti.unisa.it>
2024-07-12 16:52:37 +0200

    Si

--- src/main/webapp/script/bannerSize.js
+++ src/main/webapp/script/bannerSize.js
@@ -16,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const headerText = "I migliori corsi sul mercato";
 
     const text = [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        "Basi e approfondimenti in numerosi ambiti",
+        "Competenze utili nel mondo del lavoro",
+        "Docenti accuratamente selezionati per offrire le giuste conoscenze"
     ];
 
     // Indice per tenere traccia dell'elemento corrente dell'array
