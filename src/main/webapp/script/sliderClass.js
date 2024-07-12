077c57f202cefd8b904d4db24f46880e883c8396
RaffyManzo <raffaelemanzo1995@gmail.com>
2024-07-12 16:44:21 +0200

    Aggiunta FAQ page e Aggiunto un href al pulsante contattaci

--- src/main/webapp/script/sliderClass.js
+++ src/main/webapp/script/sliderClass.js
@@ -1,6 +1,6 @@
 class CourseSlider {
     constructor(containerSelector, url, itemsPerPage = 5) {
-        this.container = document.querySelector(containerSelector);
+        this.container = $(containerSelector);
         this.sliderEl =
             "<a class='course-box' href='course?courseID={0}'> " +
             "<div class='master-course-info'>" +
@@ -29,18 +29,20 @@ class CourseSlider {
     }
 
     init() {
-        document.addEventListener('DOMContentLoaded', () => {
+        $(document).ready(() => {
             this.loadCourses();
 
             // Gestione dei pulsanti di scorrimento
-            this.container.parentElement.querySelector(".move-slider-left").addEventListener('click', () => {
+            this.container.parent().find(" .move-slider-left").click(() => {
+                //console.log("Pressed left")
                 if (this.currentIndex > 0) {
                     this.currentIndex--;
                     this.updateSlider();
                 }
             });
 
-            this.container.parentElement.querySelector(".move-slider-right").addEventListener('click', () => {
+            this.container.parent().find(".move-slider-right").click(() => {
+                //console.log("Pressed right")
                 if (this.currentIndex < this.courseList.length - this.itemsPerPage) {
                     this.currentIndex++;
                     this.updateSlider();
@@ -48,7 +50,7 @@ class CourseSlider {
             });
 
             // Calcola il numero di elementi per pagina all'avvio e al ridimensionamento della finestra
-            window.addEventListener('resize', () => {
+            $(window).resize(() => {
                 this.updateItemsPerPage();
                 this.updateSlider();
             });
@@ -57,34 +59,54 @@ class CourseSlider {
 
     // Funzione per caricare i corsi
     loadCourses() {
+
+
+        /*$.ajax({
+            url: this.url, // Endpoint URL
+            method: 'GET',
+            dataType: 'json',
+            success: (courses) => {
+                this.courseList = courses; // Sovrascrive la lista di corsi con quella ottenuta dalla risposta
+                this.renderCourses(); // Chiama la funzione per renderizzare i corsi
+                //console.log(this.courseList)
+            },
+            error: (xhr, status, error) => {
+                // Gestione degli errori
+                alert('Errore nel recupero dei dati: ' + error);
+            }
+        });*/
+
         fetch(this.url)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Errore nel recupero dei dati: ' + response.statusText);
                 }
+                console.log('Consuming response body...');
                 return response.json();
             })
             .then(courses => {
+                console.log('Courses received:', courses);
                 this.courseList = courses; // Sovrascrive la lista di corsi con quella ottenuta dalla risposta
                 this.renderCourses(); // Chiama la funzione per renderizzare i corsi
             })
             .catch(error => {
                 // Gestione degli errori
+                console.error('Errore nel recupero dei dati:', error.message);
                 alert('Errore nel recupero dei dati: ' + error.message);
                 // Risali a 'your-courses'
                 var yourCourses = this.container.closest('.courses-section');
-                yourCourses.style.display = "none"
+                yourCourses.css("display", "none")
 
                 // Risali a 'section-header'
                 var sectionHeader = this.container.closest('.section-header');
-                sectionHeader.style.display = "none"
+                sectionHeader.css("display", "none")
             });
     }
 
     // Funzione per renderizzare i corsi
     renderCourses() {
-        this.container.innerHTML = ''; // Svuota il container prima di aggiungere i nuovi corsi
-        this.courseList.forEach((course, index) => {
+        this.container.empty(); // Svuota il container prima di aggiungere i nuovi corsi
+        $.each(this.courseList, (index, course) => {
             let formattedSliderEl = this.formatString(this.sliderEl, [
                 course.id,
                 course.name,
@@ -94,24 +116,22 @@ class CourseSlider {
                 course.creator.nome, // Accedi al nome del creator
                 course.creator.cognome // Accedi al cognome del creator
             ]);
-            this.container.insertAdjacentHTML('beforeend', formattedSliderEl);
+            this.container.append(formattedSliderEl);
         });
 
         this.updateItemsPerPage();
         this.updateSlider();
-        let courseBox = document.querySelectorAll(".course-box");
-        courseBox.forEach((box) => {
-            box.style.minWidth = "300px";
-            box.style.maxWidth = "300px";
-        });
+        let courseBox = $(".course-box");
+        courseBox.css("min-width", "300px");
+        courseBox.css("max-width", "300px");
         this.updateItemsPerPage();
         this.updateSlider();
 
         // Aggiungi l'evento error qui, dopo che gli elementi img sono stati aggiunti al DOM
-        const images = document.querySelectorAll('img:not(#profile-pic)');
-        images.forEach((img) => {
-            img.addEventListener('error', function() {
-                this.src = 'file?file=default.png&c=course';
+        const images = $('img:not(#profile-pic)');
+        images.each(function(k) {
+            $(this).on("error", function() {
+                $(this).attr('src', 'file?file=default.png&c=course');
                 imageNotFound();
             });
         });
@@ -119,21 +139,26 @@ class CourseSlider {
 
     // Funzione per calcolare il numero di elementi per pagina
     updateItemsPerPage() {
-        var windowWidth = window.innerWidth;
+        var windowWidth = $(window).width();
         var sliderWidth = (windowWidth * 0.9) - 100; // Calcola la larghezza dello slider considerando i margini
         this.itemsPerPage = Math.floor((sliderWidth / 320) - 0.8); // Ogni scheda ha una larghezza di 300px
 
         if (this.itemsPerPage <= 0) {
             this.itemsPerPage = 1;
         }
+        //console.log("Items per page:", this.itemsPerPage);
     }
 
     // Funzione per aggiornare la visibilità degli elementi
     updateSlider() {
-        var elements = Array.from(this.container.children);
-        elements.forEach((el) => el.style.display = 'none'); // Nasconde tutti gli elementi
+        var elements = this.container.children();
+        elements.hide(); // Nasconde tutti gli elementi
         for (var i = this.currentIndex; i < this.currentIndex + this.itemsPerPage && i < elements.length; i++) {
-            elements[i].style.display = 'block'; // Mostra solo gli elementi nella finestra visibile
+            $(elements[i]).show(); // Mostra solo gli elementi nella finestra visibile
         }
+
+        //console.log(this.currentIndex);
+        //console.log(this.itemsPerPage);
+        //console.log(this.courseList.length);
     }
 }
